6de2bb153a752b6222c5a4d393b7e630ae3aa4e3
iShi0n <guibs225@gmail.com>
2020-03-07 17:03:32 -0300

    some changes and add Email class

--- temp_mail.py
+++ temp_mail.py
@@ -14,17 +14,21 @@ chars = ascii_lowercase + digits
 #ascii_letters = ascii_lowercase + ascii_uppercase
 #punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
 
-class TempMail():
+class TempMail:
     def __init__(self):
         self.generate_new_email()
+
+    def get_actual_email(self) -> str:
+        """return the actual email address"""
+        return self.__actual_email
 
     def set_email(self, email: str):
         domains = self.get_available_domains()
         if email[email.index("@"):] in domains:
             email_hash = hashlib.md5(email.encode("utf-8")).hexdigest()
             
-            self.actual_email = email
-            self.actual_email_hash = email_hash
+            self.__actual_email = email
+            self.__actual_email_hash = email_hash
 
     def get_available_domains(self) -> list:
         """List all available domains"""
@@ -40,24 +44,52 @@ class TempMail():
         domains = self.get_available_domains()
         email = "".join([random.choice(ascii_lowercase) for x in range(12)])+random.choice(domains)
         email_hash = hashlib.md5(email.encode("utf-8")).hexdigest()
-        self.actual_email = email
-        self.actual_email_hash = email_hash
+        self.__actual_email = email
+        self.__actual_email_hash = email_hash
     
-    def check_inbox(self) -> "json":
+    def check_inbox(self) -> list:
         """Check if you have a new email"""
         
-        response = requests.get(f"https://api4.temp-mail.org/request/mail/id/{self.actual_email_hash}/format/json", headers={"User-Agent": "okhttp/3.12.6"})
+        response = requests.get(f"https://api4.temp-mail.org/request/mail/id/{self.__actual_email_hash}/format/json", headers={"User-Agent": "okhttp/3.12.6"})
         response = json.loads(response.text)
 
         try:
             if "error" in response.keys(): #json with emails is a list
-                return response
+                return []
         except AttributeError:
-            emails_json = json.loads('{"emails":[]}')
+            emails = []
             for i in response:
-                json_to_append = json.loads(json.dumps({"from":i["mail_from"], "subject":i["mail_subject"], "mail_text":i["mail_text"]}))
-                emails_json["emails"].append(json_to_append)
-            return emails_json
+                emails.append(Email(i["mail_from"], i["mail_subject"], i["mail_text"]))
+            
+            return emails
 
+
+class Email:
+    def __init__(self, _from: str, subject: str, body: str):
+        self.__from = _from
+        self.__subject = subject
+        self.__body = body
+
+    def get_subject(self) -> str:
+        """return email's subject"""
+        
+        return self.__subject
+
+    def get_sender(self) -> str:
+        """return who sent the email"""
+        
+        return self.__from
+
+    def get_text(self) -> str:
+        """return email's body"""
+        
+        return self.__body
+
+    def get_all(self) -> dict:
+        """return email's infos in a dict"""
+        
+        return {"from":self.__from, "subject": self.__subject, "body": self.__body}
+    
+    
 
             
