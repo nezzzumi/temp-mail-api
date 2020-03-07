37f84b325d05f60aa72c9b9b69529dfc87eb6266
iShi0n <guibs225@gmail.com>
2020-03-07 00:11:40 -0300

    first commit

---
+++ temp_mail.py
@@ -0,0 +1,69 @@
+#EternalChaos - 06/03/2020
+
+"""
+import temp_mail
+t = temp_mail.TempMail()
+t.actual_email
+t.check_inbox()
+"""
+
+import requests
+import json
+import random
+import hashlib
+from time import ctime
+from bs4 import BeautifulSoup as bs
+
+
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+chars = ascii_lowercase + digits
+#ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#ascii_letters = ascii_lowercase + ascii_uppercase
+#punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+
+class TempMail():
+    def __init__(self):
+        self.generate_new_email()
+
+    def get_available_domains(self) -> list:
+        """List all available domains"""
+
+        response = requests.get("https://api4.temp-mail.org/request/domains/format/json", headers={"User-Agent": "okhttp/3.12.6"})
+        domains = json.loads(response.text)
+        
+        return list(domains)
+
+    def generate_new_email(self):
+        """Generate an new temp-email"""
+
+        domains = self.get_available_domains()
+        email = "".join([random.choice(ascii_lowercase) for x in range(12)])+random.choice(domains)
+        email_hash = hashlib.md5(email.encode("utf-8")).hexdigest()
+        self.actual_email = email
+        self.actual_email_hash = email_hash
+    
+    def check_inbox(self) -> dict:
+        """Check if you have a new email
+        
+        Raises:
+            Exception: 
+        """
+        if self.actual_email == None:
+            raise Exception(": You don't have an email yet.")
+        
+        response = requests.get(f"https://api4.temp-mail.org/request/mail/id/{self.actual_email_hash}/format/json", headers={"User-Agent": "okhttp/3.12.6"})
+        response = json.loads(response.text)
+
+        try:
+            if "error" in response.keys(): #json with emails is a list
+                return response
+        except AttributeError:
+            emails_json = json.loads('{"emails":[]}')
+            for i in response:
+                json_to_append = json.loads(json.dumps({"from":i["mail_from"], "subject":i["mail_subject"], "mail_text":i["mail_text"]}))
+                emails_json["emails"].append(json_to_append)
+            return emails_json
+
+
+            
